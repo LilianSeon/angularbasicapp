d50d28047d3e5295f00706d471a21b5c56254448
LilianSeon <eragnylilian@live.fr>
2025-08-02 18:26:42 +0200

    PrimeNG + Tailwind

--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -1,25 +1,30 @@
-import { Component, EventEmitter, input, model, output, Output } from '@angular/core';
+import { Component, EventEmitter, input, model, output, Output, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AutoCompleteCompleteEvent, AutoCompleteModule } from 'primeng/autocomplete';
+import { Monster } from '../../models/monster.model';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [FormsModule],
+  imports: [FormsModule, AutoCompleteModule],
   templateUrl: './search-bar.html',
-  styleUrl: './search-bar.css'
+  styleUrl: './search-bar.css',
+  //encapsulation: ViewEncapsulation.None
 })
 export class SearchBar {
 
   //search = input('Inital value');
-  search = model('Inital value'); // emit() new value each time it changes + create input & output `search`
+  search = input('Inital value'); // model() => emit() new value each time it changes + create input & output `search`
+  filteredMonsters = input<Monster[]>([new Monster()]);
 
   @Output() searchButtonClick = new EventEmitter();
   //searchChange = output<string>({ alias: 'submit' });
 
-  searchClick() {
-    this.searchButtonClick.emit();
+  searchClick(e: AutoCompleteCompleteEvent) {
+    this.searchButtonClick.emit(e.query);
+    console.log('searchClick', e)
   }
 
   updateSearch(value: string) {
-    this.search.set(value);
+    //this.search.set(value);
   }
 }
